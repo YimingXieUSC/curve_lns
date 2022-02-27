5ac334a3500a9d7582bca6310ef13e0f6a117417
Trojan <trojan@vm356>
2022-02-27 15:25:05 -0800

    change automation script for larger size testing

--- automation.sh
+++ automation.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 echo "Running Experiments: Grid Collision"
 
-name="random-32-32-20"
+name="warehouse-20-40-10-2-2-10000agents"
 map="/home/trojan/Desktop/curve/MAPF-LNS2/maps/$name.map"
-scen1="random"
-scen="/home/trojan/Desktop/curve/MAPF-LNS2/scen_$scen1/$name-$scen1"
+scen="/home/trojan/Desktop/curve/MAPF-LNS2/instances/$name"
 output="/home/trojan/Desktop/curve/MAPF-LNS2/$name/GRID.csv-LNS/$name-$scen1"
 sid=0
 time=60
 w=1.02
 
-echo "$n agents on instance $name-$scen1-$i  w=$w $scen-$i.scen"
-echo "destination: "
-/home/trojan/Desktop/curve/MAPF-LNS2/lns -m $map -a $scen-$i.scen -k $n -o $output-$n-$w-$sid-GRID.csv -t $time
+for i in $(seq 1 1 25)
+do
+    echo "$n agents on instance $name-$scen1-$i  w=$w $scen-$i.scen"
+    echo "destination: "
+    /home/trojan/Desktop/curve/MAPF-LNS2/build/lns -m $map -a $scen-$i.scen -k $n -o $output-$n-$w-$sid-GRID.csv -t $time
+done
+
 
 # for n in $(seq 20 20 100)
 # do
